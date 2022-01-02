4ab8159d61eee0c538347ff1e1d709b53b6ebdcc
PrzeZ <przemyslawzykubek@gmail.com>
2022-01-02 14:14:38 +0100

    Added creating bitmap

--- MusicApp/Form1.cs
+++ MusicApp/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PdfSharp.Drawing;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -27,7 +28,38 @@ namespace MusicApp
 
         private void TextBox1_TextChanged(object sender, EventArgs e)
         {
+<<<<<<< Updated upstream
             musicSheet.SetText(textBox1.Text);
+=======
+            musicSheet.SetNotes(textBox1.Text);
+            CreateBitmapAtRuntime();
+        }
+
+        public void CreateBitmapAtRuntime()
+        {
+            PictureBox pictureBox = new PictureBox();
+            pictureBox1.Size = new Size(700, 500);
+            this.Controls.Add(pictureBox);
+
+            Bitmap bitmap = new Bitmap(700, 500);
+            Graphics graphics = Graphics.FromImage(bitmap);
+
+            int red = 0;
+            int white = 11;
+
+            graphics.FillRectangle(Brushes.Red, 0, red, 700, 500);
+            pictureBox1.Image = bitmap;
+        }
+
+        void DrawImage(XGraphics gfx, int number)
+        {
+
+            XImage image = XImage.FromFile(/*jpegSamplePath*/);
+
+            // Left position in point
+            double x = (250 - image.PixelWidth * 72 / image.HorizontalResolution) / 2;
+            gfx.DrawImage(image, x, 0);
+>>>>>>> Stashed changes
         }
     }
 }
--- MusicApp/MusicSheet.cs
+++ MusicApp/MusicSheet.cs
@@ -13,7 +13,26 @@ namespace MusicApp
 
         public void SetText(string text)
         {
+<<<<<<< Updated upstream
             inputText = text;
+=======
+            notes = ConvertTextToNotes(text);
+        }
+
+        private List<Note> ConvertTextToNotes(string text)
+        {
+            List<Note> notes = new List<Note>();
+            string[] parts = text.Split(new char[notes.Count]);
+            foreach (var part in parts)
+            {
+                if (part == "c3")
+                { 
+                    notes.Add(new Note("c3"));
+                }
+            }
+            
+            return notes;
+>>>>>>> Stashed changes
         }
     }
 }
