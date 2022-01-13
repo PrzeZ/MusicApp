5d723e5f5f60f56c9ba042b342046de2d35b4efb
PrzeZ <56115183+PrzeZ@users.noreply.github.com>
2022-01-13 22:37:47 +0100

    General cleanup

--- MusicApp/PDFConverter.cs
+++ MusicApp/PDFConverter.cs
@@ -1,10 +1,7 @@
 ﻿using PdfSharp.Drawing;
 using PdfSharp.Pdf;
 using System;
-using System.Collections.Generic;
 using System.Drawing;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
